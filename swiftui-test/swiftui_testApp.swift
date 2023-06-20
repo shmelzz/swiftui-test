a560d95b336821ddccb7ba37a3639d7a41dd0f37
Elizaveta Shelemekh <shelemekhliza0313@gmail.com>
2023-06-20 12:05:57 +0300

    Initial Commit

---
+++ swiftui-test/swiftui_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_testApp.swift
+//  swiftui-test
+//
+//  Created by Elizaveta Shelemekh on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
