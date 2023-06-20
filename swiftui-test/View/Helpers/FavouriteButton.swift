224e10b0c796d14638a4c99a4cef724152cdcd47
Elizaveta Shelemekh <shelemekhliza0313@gmail.com>
2023-06-20 22:36:31 +0300

    complete chapter-1

---
+++ swiftui-test/View/Helpers/FavouriteButton.swift
@@ -0,0 +1,29 @@
+//
+//  FavouriteButton.swift
+//  swiftui-test
+//
+//  Created by Elizaveta Shelemekh on 20.06.2023.
+//
+
+import SwiftUI
+
+struct FavouriteButton: View {
+    
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavouriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavouriteButton(isSet: .constant(true))
+    }
+}
---
+++ swiftui-test/View/Landmarks/LandmarkList.swift
@@ -0,0 +1,49 @@
+//
+//  LandmarkList.swift
+//  swiftui-test
+//
+//  Created by Elizaveta Shelemekh on 20.06.2023.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach (filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+                .environmentObject(ModelData())
+        }
+    }
+}
