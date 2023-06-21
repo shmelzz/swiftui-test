c7004300d53b30f059e8dd987ce81880b5559fbc
Elizaveta Shelemekh <shelemekhliza0313@gmail.com>
2023-06-21 13:41:42 +0300

    add badges

---
+++ swiftui-test/View/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  swiftui-test
+//
+//  Created by Elizaveta Shelemekh on 21.06.2023.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
